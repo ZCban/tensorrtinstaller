7f25f9a1b43fbca3eb76d80813e9b616f58993ab
ZCban <100644497+ZCban@users.noreply.github.com>
2024-03-12 08:21:48 +0100

    Create apytorch.py

---
+++ apytorch.py
@@ -0,0 +1,7 @@
+
+
+# Disinstallazione e installazione di torch, torchvision, e torchaudio
+subprocess.run(['pip', 'uninstall', 'torch', 'torchvision', 'torchaudio', '-y'])
+print("Disinstallazione di torch, torchvision e torchaudio completata.")
+subprocess.run(['pip3', 'install', 'torch', 'torchvision', 'torchaudio', '--index-url', 'https://download.pytorch.org/whl/cu121'])
+print("Installazione delle versioni nightly di torch, torchvision e torchaudio completata.")
